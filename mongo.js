a393243a20f40df5b45f52ff06313c520c89a3a7
PremNodejsdeveloper <premprakash1519@gmail.com>
2020-06-27 13:21:56 +0530

    changes to run the app atleast

--- mongo.js
+++ mongo.js
@@ -1,37 +1,37 @@
-const { ObjectID } = require("mongodb");
+// const { ObjectID } = require("mongodb");
 
-const MongoClient = require("mongodb").MongoClient;
-const ObjcetId = require("mongodb").ObjectID;
-const url = "mongodb://localhost:27017/authorDb";
-const mongoOptions = { useNewUrlParser: true };
+// const MongoClient = require("mongodb").MongoClient;
+// const ObjcetId = require("mongodb").ObjectID;
+// const url = "mongodb://localhost:27017/authorDb";
+// const mongoOptions = { useNewUrlParser: true };
 
-const state = {
-  db: null,
-};
+// const state = {
+//   db: null,
+// };
 
-const connect = (cb) => {
-  if (state.db) cb();
-  else {
-    MongoClient.connect(url, mongoOptions, (err, client) => {
-      if (err) {
-        cb(err);
-      } else {
-        state.db = client.db("authorDb");
-        cb();
-      }
-    });
-  }
-};
-const getPrimarykey = (_id) => {
-  return ObjectID(_id);
-};
+// const connect = (cb) => {
+//   if (state.db) cb();
+//   else {
+//     MongoClient.connect(url, mongoOptions, (err, client) => {
+//       if (err) {
+//         cb(err);
+//       } else {
+//         state.db = client.db("authorDb");
+//         cb();
+//       }
+//     });
+//   }
+// };
+// const getPrimarykey = (_id) => {
+//   return ObjectID(_id);
+// };
 
-const getDB = () => {
-  return state.db;
-};
+// const getDB = () => {
+//   return state.db;
+// };
 
-module.exports = {
-  getDB,
-  connect,
-  getPrimarykey,
-};
+// module.exports = {
+//   getDB,
+//   connect,
+//   getPrimarykey,
+// };
